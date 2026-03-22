c24dfff839381f93809a9a90d66138032a4a575e
Ahmedsayed322 <mamdatap@gmail.com>
2026-03-22 14:20:44 +0200

    My Store v1

--- src/app/services/seo-service.ts
+++ src/app/services/seo-service.ts
@@ -18,7 +18,7 @@ export class SeoService {
     this.meta.updateTag({ property: 'og:site_name', content: this.siteName });
     this.meta.updateTag({ property: 'og:title', content: seoData.title });
     this.meta.updateTag({ property: 'og:description', content: seoData.description });
-    this.meta.updateTag({ property: 'og:image:width', content: '1200' });
+    this.meta.updateTag({ property: 'og:image:height', content: '1200' });
     this.meta.updateTag({ property: 'og:image:width', content: '640' });
     this.meta.updateTag({ property: 'og:locale', content: 'en_US' });
     this.meta.updateTag({ property: 'og:image', content: imageUrl });
